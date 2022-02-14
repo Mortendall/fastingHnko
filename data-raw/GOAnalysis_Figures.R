d194e2970447d86ae24d3ce1ca9b2baa1aeb4421
Morten Dall <mortendall1988@gmail.com>
2022-02-14 16:29:13 +0100

    expanded analysis. Started analyzing overlap and uniquely DEGs.

--- data-raw/GOAnalysis_Figures.R
+++ data-raw/GOAnalysis_Figures.R
@@ -44,7 +44,7 @@ upsetRNA <- upsetRNA + ggplot2::ggtitle("Upsetplot")+ggplot2::theme(plot.title =
                                                                                               hjust = 0.5,
                                                                                               vjust = 0.95))
 
-
+upsetRNA
  # tiff("UpsetRNA.tif", unit = "cm", height = 25, width = 50, res = 600)
  # upsetRNA
  # grid::grid.text("Dif. expressed genes", x=0.65, y = 0.95, gp=grid::gpar(fontsize = 30))
@@ -59,4 +59,269 @@ go_sig_genes <- goAnalysis(edgeR_data, "MF", "")
 go_sig_genes_cc <- goAnalysis(edgeR_data, "CC", "")
 #printGOterms(go_sig_genes_cc)
 
+for (i in 1:length(go_sig_genes)){
+    go_sig_genes[[i]] <- clusterProfiler::setReadable(go_sig_genes[[i]],
+                                                    OrgDb = org.Mm.eg.db, keyType = "ENTREZID")
+}
 
+for (i in 1:length(go_sig_genes_cc)){
+    go_sig_genes_cc[[i]] <- clusterProfiler::setReadable(go_sig_genes_cc[[i]],
+                                                      OrgDb = org.Mm.eg.db, keyType = "ENTREZID")
+}
+
+#extract data from the organelle inner membrane GO term for heatmap
+go_sig_genes_cc$Genotype_long$Description[4]
+organelle_inner_membrane <- go_sig_genes_cc$Genotype_long$geneID[4]
+organelle_inner_membrane <- unlist(str_split(organelle_inner_membrane, "/"))
+
+res_ox <- cpm_matrix %>%
+    dplyr::filter(SYMBOL %in% organelle_inner_membrane) %>%
+    dplyr::distinct(SYMBOL, .keep_all = T)
+rownames(res_ox) <- res_ox$SYMBOL
+res_ox <- res_ox %>%
+    dplyr::select(-SYMBOL)
+
+setup_ordered <- metadata
+setup_ordered <- setup_ordered %>%
+    dplyr::mutate(
+        Group = dplyr::case_when(
+            Group == "WT_18h" ~ "WT 18h",
+            Group == "KO_18h" ~ "HNKO 18h",
+            Group == "WT_5h" ~ "WT 5h",
+            Group == "KO_5h" ~ "HNKO 5h"
+        )
+    )
+order <- c("WT 5h", "HNKO 5h", "WT 18h", "HNKO 18h")
+
+setup_ordered <- setup_ordered %>%
+    dplyr::arrange(desc(Fast),desc(Genotype))
+
+setup_ordered$Sample <- as.character(setup_ordered$Sample)
+res_ox <- res_ox %>%
+    dplyr::select(setup_ordered$Sample)
+
+key <- as.data.frame(setup_ordered)
+
+key <- key %>%
+    dplyr::select(Group)
+rownames(key) <- setup_ordered$Sample
+key$Group <- factor(key$Group, c("WT 5h", "HNKO 5h", "WT 18h", "HNKO 18h"))
+
+
+
+Heatmap <- pheatmap::pheatmap(res_ox,
+                            treeheight_col = 0,
+                            treeheight_row = 0,
+                            scale = "row",
+                            legend = T,
+                            na_col = "white",
+                            Colv = NA,
+                            na.rm = T,
+                            cluster_cols = F,
+                            fontsize_row = 3,
+                            fontsize_col = 8,
+                            cellwidth = 7,
+                            cellheight = 3,
+                            annotation_col = key,
+                            show_colnames = F,
+                            show_rownames = T,
+                            main = "Organelle Inner Membrane"
+)
+
+#####run GO analysis on data unique for various categories####
+#analyze common genotype data
+long_genotype <- upset_data |>
+    dplyr::filter(Group=="Genotype_long")
+short_genotype <- upset_data |>
+    dplyr::filter(Group=="Genotype_short")
+overlap <- long_genotype |>
+    dplyr::filter(Gene %in% short_genotype$Gene)
+
+bg <- cpm_matrix$SYMBOL
+bg_list <- clusterProfiler::bitr(
+    bg,
+    fromType = "SYMBOL",
+    toType = "ENTREZID",
+    OrgDb = "org.Mm.eg.db",
+    drop = T
+)
+
+
+genes <- unlist(str_split(overlap$Gene, "/"))
+
+gene_list <- clusterProfiler::bitr(
+    genes,
+    fromType = "SYMBOL",
+    toType = "ENTREZID",
+    OrgDb = "org.Mm.eg.db",
+    drop = T
+)
+
+goResults_MF <- clusterProfiler::enrichGO(gene = gene_list$ENTREZID,
+                                       universe = bg_list$ENTREZID,
+                                       OrgDb = org.Mm.eg.db,
+                                       ont = "MF")
+goResults_CC <- clusterProfiler::enrichGO(gene = gene_list$ENTREZID,
+                                          universe = bg_list$ENTREZID,
+                                          OrgDb = org.Mm.eg.db,
+                                         ont = "CC")
+#create heatmap on 100 random genes
+res_ox <- cpm_matrix %>%
+    dplyr::filter(SYMBOL %in% genes) %>%
+    dplyr::distinct(SYMBOL, .keep_all = T) |>
+    dplyr::slice(1:100)
+res_ox <- res_ox |>
+    dplyr::filter(!is.na(SYMBOL))
+rownames(res_ox) <- res_ox$SYMBOL
+res_ox <- res_ox %>%
+    dplyr::select(-SYMBOL)
+
+setup_ordered <- metadata
+setup_ordered <- setup_ordered %>%
+    dplyr::mutate(
+        Group = dplyr::case_when(
+            Group == "WT_18h" ~ "WT 18h",
+            Group == "KO_18h" ~ "HNKO 18h",
+            Group == "WT_5h" ~ "WT 5h",
+            Group == "KO_5h" ~ "HNKO 5h"
+        )
+    )
+order <- c("WT 5h", "HNKO 5h", "WT 18h", "HNKO 18h")
+
+setup_ordered <- setup_ordered %>%
+    dplyr::arrange(desc(Fast),desc(Genotype))
+
+setup_ordered$Sample <- as.character(setup_ordered$Sample)
+res_ox <- res_ox %>%
+    dplyr::select(setup_ordered$Sample)
+
+key <- as.data.frame(setup_ordered)
+
+key <- key %>%
+    dplyr::select(Group)
+rownames(key) <- setup_ordered$Sample
+key$Group <- factor(key$Group, c("WT 5h", "HNKO 5h", "WT 18h", "HNKO 18h"))
+
+
+
+Heatmap <- pheatmap::pheatmap(res_ox,
+                              treeheight_col = 0,
+                              treeheight_row = 0,
+                              scale = "row",
+                              legend = T,
+                              na_col = "white",
+                              Colv = NA,
+                              na.rm = T,
+                              cluster_cols = F,
+                              fontsize_row = 3,
+                              fontsize_col = 8,
+                              cellwidth = 7,
+                              cellheight = 3,
+                              annotation_col = key,
+                              show_colnames = F,
+                              show_rownames = T,
+                              main = "100 genes w. genotype regardless of fast"
+)
+#analysis of long fast genes
+long_genotype <- upset_data |>
+    dplyr::filter(Group=="Genotype_long")
+short_genotype <- upset_data |>
+    dplyr::filter(Group=="Genotype_short")
+
+unique_long <- long_genotype |>
+    dplyr::filter(!Gene %in% short_genotype$Gene)
+
+bg <- cpm_matrix$SYMBOL
+bg_list <- clusterProfiler::bitr(
+    bg,
+    fromType = "SYMBOL",
+    toType = "ENTREZID",
+    OrgDb = "org.Mm.eg.db",
+    drop = T
+)
+
+
+genes <- unlist(str_split(unique_long$Gene, "/"))
+
+gene_list <- clusterProfiler::bitr(
+    genes,
+    fromType = "SYMBOL",
+    toType = "ENTREZID",
+    OrgDb = "org.Mm.eg.db",
+    drop = T
+)
+
+goResults_MF <- clusterProfiler::enrichGO(gene = gene_list$ENTREZID,
+                                          universe = bg_list$ENTREZID,
+                                          OrgDb = org.Mm.eg.db,
+                                          ont = "MF")
+goResults_CC <- clusterProfiler::enrichGO(gene = gene_list$ENTREZID,
+                                          universe = bg_list$ENTREZID,
+                                          OrgDb = org.Mm.eg.db,
+                                          ont = "CC")
+
+goResults_CC <- clusterProfiler::setReadable(goResults_CC, OrgDb = org.Mm.eg.db, keyType = "ENTREZID")
+
+goResults_CC$Description[4]
+organelle_inner_membrane <- goResults_CC$geneID[4]
+organelle_inner_membrane <- unlist(str_split(organelle_inner_membrane, "/"))
+
+res_ox <- cpm_matrix %>%
+    dplyr::filter(SYMBOL %in% organelle_inner_membrane) %>%
+    dplyr::distinct(SYMBOL, .keep_all = T)
+res_ox <- res_ox |>
+    dplyr::filter(!is.na(SYMBOL))
+rownames(res_ox) <- res_ox$SYMBOL
+res_ox <- res_ox %>%
+    dplyr::select(-SYMBOL)
+
+setup_ordered <- metadata
+setup_ordered <- setup_ordered %>%
+    dplyr::mutate(
+        Group = dplyr::case_when(
+            Group == "WT_18h" ~ "WT 18h",
+            Group == "KO_18h" ~ "HNKO 18h",
+            Group == "WT_5h" ~ "WT 5h",
+            Group == "KO_5h" ~ "HNKO 5h"
+        )
+    )
+order <- c("WT 5h", "HNKO 5h", "WT 18h", "HNKO 18h")
+
+setup_ordered <- setup_ordered %>%
+    dplyr::arrange(desc(Fast),desc(Genotype))
+
+setup_ordered$Sample <- as.character(setup_ordered$Sample)
+res_ox <- res_ox %>%
+    dplyr::select(setup_ordered$Sample)
+
+key <- as.data.frame(setup_ordered)
+
+key <- key %>%
+    dplyr::select(Group)
+rownames(key) <- setup_ordered$Sample
+key$Group <- factor(key$Group, c("WT 5h", "HNKO 5h", "WT 18h", "HNKO 18h"))
+
+
+
+Heatmap <- pheatmap::pheatmap(res_ox,
+                              treeheight_col = 0,
+                              treeheight_row = 0,
+                              scale = "row",
+                              legend = T,
+                              na_col = "white",
+                              Colv = NA,
+                              na.rm = T,
+                              cluster_cols = F,
+                              fontsize_row = 5,
+                              fontsize_col = 8,
+                              cellwidth = 7,
+                              cellheight = 5,
+                              annotation_col = key,
+                              show_colnames = F,
+                              show_rownames = T,
+                              main = "Organelle Inner Membrane \n Significantly different for long fast "
+)
+
+# tiff(here::here("Heatmap_inner_membrane_long_fast.tif"), unit = "cm", height = 25, width = 50, res = 600)
+# Heatmap
+# dev.off()
